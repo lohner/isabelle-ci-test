b6ca1fa883a48445d2e26cf5686ac02488193707
Denis Lohner <denis.lohner@kit.edu>
2016-03-01 14:33:41 +0100

    add another lemma

--- Test.thy
+++ Test.thy
@@ -2,4 +2,6 @@ theory Test imports Main begin
 
 lemma imp_refl: "A \<longrightarrow> A" by simp
 
+lemma imp_trans: "\<lbrakk> A \<longrightarrow> B; B \<longrightarrow> C \<rbrakk> \<Longrightarrow> A \<longrightarrow> C" by simp
+
 end

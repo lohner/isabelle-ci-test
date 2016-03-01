205a85f74ff7e44dc5b46c3f2267c6ce276bc235
Denis Lohner <denis.lohner@kit.edu>
2016-03-01 17:05:56 +0100

    add another lemma

--- Test.thy
+++ Test.thy
@@ -4,4 +4,6 @@ lemma imp_refl: "A \<longrightarrow> A" by simp
 
 lemma imp_trans: "\<lbrakk> A \<longrightarrow> B; B \<longrightarrow> C \<rbrakk> \<Longrightarrow> A \<longrightarrow> C" by simp
 
+lemma imp_antisym: "\<lbrakk> A \<longrightarrow> B; B \<longrightarrow> A \<rbrakk> \<Longrightarrow> A = B" by auto
+
 end

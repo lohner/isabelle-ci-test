317facc94d8dbe27e0d640c21214384337112d9f
Denis Lohner <denis.lohner@kit.edu>
2016-03-02 09:19:54 +0100

    Test with AFP

--- Test.thy
+++ Test.thy
@@ -1,9 +1,15 @@
-theory Test imports Main begin
+theory Test imports
+  Main
+  "$AFP/AVL-Trees/AVL"
+begin
 
 lemma imp_refl: "A \<longrightarrow> A" by simp
 
 lemma imp_trans: "\<lbrakk> A \<longrightarrow> B; B \<longrightarrow> C \<rbrakk> \<Longrightarrow> A \<longrightarrow> C" by simp
 
 lemma imp_antisym: "\<lbrakk> A \<longrightarrow> B; B \<longrightarrow> A \<rbrakk> \<Longrightarrow> A = B" by auto
+
+definition mytree :: "nat tree"
+  where "mytree = ET"
 
 end
